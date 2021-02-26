081db316d461f40a60d9770519d128d3f2546e64
farahwz <farahwz@upi.edu>
2021-02-26 09:15:30 +0700

    farahwz 1905603

---
+++ Aktifitas1.py
@@ -0,0 +1,11 @@
+#INPUT
+
+nama=input("masukkan nama Anda: Farahwz")
+umur=19
+
+#OUTPUT
+print("")
+print("====INPUT DAN OUTPUT PADA BAHASA PEMROGRAMAN PYTHON====")
+print("nama Anda: Farah",nama)
+print("umur Anda: 19",umur)
+print("")
