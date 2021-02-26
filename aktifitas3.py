ab56d700410a249d95fc9b208b8eb35250f4fd00
farahwz <farahwz@upi.edu>
2021-02-26 10:02:13 +0700

    aktifitas3

---
+++ aktifitas3.py
@@ -0,0 +1,14 @@
+#PERCABANGAN
+print("percabangan pada bahasa pemrograman Phyton")
+print("pilih bahasa pemrograman: ")
+print("1. Python")
+print("2. C")
+
+pilih=int(input("pilih menu (1/2) :"))
+
+if pilih ==1:
+    print("bahasa pemrograman yang dipilih adalah Phyton")
+elif pilih ==2:
+    print("bahasa pemrograman yang dipilih adalah C")
+else :
+    print("pilihan Anda tidak tersedia!!!")
